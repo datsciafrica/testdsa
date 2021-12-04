e26e2a7fc776bab630a8651665de08795f52d2ed
bitwirealbert <bitwire.albert@gmail.com>
2021-12-04 19:25:58 +0300

    Updated the index page and events

--- assets/js/news.js
+++ assets/js/news.js
@@ -12,8 +12,8 @@
 	    dots: true,
 	    autoplayHoverPause: false,
 	    items: 3,
-	    //navText : ["<span class='fa fa-user'></span>","<span class='fa fa-user'></span>"],
-        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+	    navText : ['<i class="flaticon-left-arrow-1"></i>', '<i class="flaticon-right-arrow-1"></i>'],
+        //navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
 	    responsive:{
 	      0:{
 	        items:1
